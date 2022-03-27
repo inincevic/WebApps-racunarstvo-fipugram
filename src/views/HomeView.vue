de0c830ad87e18be889ffb00016e0bd62df29d6b
inincevic <inincevic@unipu.hr>
2022-03-27 19:26:06 +0200

    Edited home screen so that it shows "stories"

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,31 @@
 <template>
-    <div class="row">
-      <div class="col-2"></div>
-      <div class="col-7">... ovdje ide Instagram story!</div>
-      <div class="col-3">... ovdje ide desni stupac!</div>
+  <div class="row">
+    <div class="col-2"></div>
+    <div class="col-7">
+      <instagram-card />
+      <instagram-card />
+      <instagram-card />
+      <instagram-card />
+      <instagram-card />
+      <instagram-card />
+      <instagram-card />
+      <instagram-card />
+      <instagram-card />
+      <instagram-card />
+      <instagram-card />
     </div>
+    <div class="col-3"> Sidebar </div>
+  </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import InstagramCard from "@/components/InstagramCard.vue";
 
 export default {
   name: "HomeView",
   components: {
-    HelloWorld,
+    InstagramCard,
   },
 };
 </script>
