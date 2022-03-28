5a9500a82b47a8babec5b15d92c949f3407485c2
inincevic <inincevic@unipu.hr>
2022-03-28 12:20:14 +0200

    Added in firebase and fixed navbar collapse

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import 'firebase/auth';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCLTpLXJAmx20UYeOmmDzanhPu0ArKwpdk",
+    authDomain: "fipugram-943b1.firebaseapp.com",
+    projectId: "fipugram-943b1",
+    storageBucket: "fipugram-943b1.appspot.com",
+    messagingSenderId: "476699345677",
+    appId: "1:476699345677:web:68b091dc1502544deb6644"
+  };
+  
+  // Initialize Firebase
+  export default firebase = initializeApp(firebaseConfig);
--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -7,26 +7,77 @@
         <div class="col-sm">
           <form>
             <div class="form-group">
-              <input type="first-name" class="form-control" id="exampleInputFirstName1" aria-describedby="firstNameHelp" placeholder="Enter first name" />
+              <input
+                type="email"
+                v-model="username"
+                class="form-control"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+                placeholder="Enter username"
+              />
             </div>
+            
+            <br>
+
             <div class="form-group">
-              <input type="last-name" class="form-control" id="exampleInputLastName1" aria-describedby="lastNameHelp" placeholder="Enter last name" />
+              <input
+                type="password"
+                v-model="password"
+                class="form-control"
+                id="exampleInputPassword1"
+                placeholder="Password"
+              />
             </div>
+
+            <br>
+
             <div class="form-group">
-              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
-              <small id="emailHelp" class="form-text text-muted" >We'll never share your email with anyone else.</small>
+              <input
+                type="password"
+                v-model="passwordConfirmation"
+                class="form-control"
+                id="exampleInputPassword2"
+                placeholder="Confirm Password"
+              />
             </div>
-            <div class="form-group">
-              <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" />
-            </div>
-            <div class="form-group">
-              <input type="password" class="form-control" id="exampleInputPassword2" placeholder="Confirm Password" />
-            </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
+            <button type="button" @click="signup" class="btn btn-primary">Submit</button>
           </form>
         </div>
         <div class="col-sm"></div>
-        </div>
       </div>
     </div>
+  </div>
 </template>
+
+
+<script>
+import { initializeApp } from 'firebase/app';
+
+
+export default {
+  name: 'Signup',
+  data() {
+    return {
+      username: '',
+      password: '',
+      passwordConfirmation: '',
+    }
+  },
+  methods: {
+    signup() {
+      if(this.password == this.passwordConfirmation) {
+        initializeApp
+        .auth()
+        .createUserWithEmailAndPassword(this.username, this.password)
+        .then(function() {
+          console.log("Registration successful");
+        })
+        .catch(function(error) {
+          console.error("Doslo je do greske", error)
+        })
+      }
+      else altert("Passwords do not match");
+    }
+  }
+}
+</script>
